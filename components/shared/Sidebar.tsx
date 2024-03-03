593976d85db9b4168601b4b606dc9fbbe5ad3322
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-04 02:23:38 +0530

    sidebar.tsx-signup

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -2,9 +2,10 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { SignedIn } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import { navLinks } from '@/constants'
 import { usePathname } from 'next/navigation'
+import { Button } from '../ui/button'
 const Sidebar = () => {
   const pathname = usePathname();
   return (
@@ -27,8 +28,16 @@ const Sidebar = () => {
                   </li>
                 )
               })}
+            <li className='flex-center cursor-pointer gap-2 p-4'>
+              <UserButton  afterSignOutUrl='/' showName/>
+            </li>
             </ul>
           </SignedIn>
+          <SignedOut >
+              <Button asChild className='button bg-purple-gradient bg-cover'>
+                <Link href="/sign-in">Login</Link>
+              </Button>
+          </SignedOut>
         </nav>
         </div>
         
