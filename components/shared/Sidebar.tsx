90a6d50fb633a69e8800389decf21149528f30c1
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-04 22:33:41 +0530

    Sidebar.tsx

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -12,12 +12,24 @@ const Sidebar = () => {
     <aside className='sidebar'>
         <div className='flex size-full flex-col gap-4 '>
         <Link href="/" className='sidebar-logo'>
-            <Image src="/assets/images/logo-text.png" alt='logo' width={180} height={280} />
+            <Image src="/assets/images/logo-text.png" alt='logo' width={180} height={28} style={{borderRadius:'10px'}}/>
         </Link>
         <nav className='sidebar-nav'>
           <SignedIn>
             <ul className='sidebar-nav_elements'>
-              {navLinks.map((link)=>{
+              {navLinks.slice(0,6).map((link)=>{
+                const isActive = link.route ===pathname
+                return (
+                  <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' :'text-gray-700'}`}>
+                    <Link className='sidebar-link' href={link.route}>
+                        <Image src={link.icon} alt='logo' width={24} height={24} className={`${isActive && 'brightness-200'}`} />
+                        {link.label}
+                    </Link>
+                  </li>
+                )
+              })}</ul>
+            <ul className='sidebar-nav_elements' >
+            {navLinks.slice(6).map((link)=>{
                 const isActive = link.route ===pathname
                 return (
                   <li key={link.route} className={`sidebar-nav_element group ${isActive ? 'bg-purple-gradient text-white' :'text-gray-700'}`}>
