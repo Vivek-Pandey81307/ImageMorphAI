924732bfef52c7fd31d86219c6adfef03b16b24f
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-08 15:19:52 +0530

    transformations>add>type>page.tsx

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -2,14 +2,21 @@ import Header from '@/components/shared/Header'
 import React from 'react'
 import {transformationTypes} from '@/constants/index'
 import TransformationForm from '@/components/shared/TransformationForm';
-const AddTransformationTypePage = ({params:{type }} :SearchParamProps) => {
+import { auth } from '@clerk/nextjs';
+import { getUserById } from '@/lib/actions/user.actions';
+import { redirect } from 'next/navigation';
+const AddTransformationTypePage = async({params:{type }} :SearchParamProps) => {
   const transformation = transformationTypes[type];
+  const {userId} = auth();
+  if(!userId) redirect('/sign-in')
 
+  const user = await getUserById(userId);
   return (
     <>
       <Header title={transformation.title} 
     subtitle={transformation.subTitle} />
-    <TransformationForm />
+    <TransformationForm action="Add" userId={user._id} type={transformation.type as TransformationTypeKey}
+    creditBalance={user.creditBalance}/>
     </>
   )
 }
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -2,10 +2,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-});
+import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -16,20 +13,32 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-const TransformationForm = () => {
+import { defaultValues } from "@/constants";
+const formSchema = z.object({
+  title : z.string(),
+  aspectRatio : z.string().optional(),
+  color : z.string().optional(),
+  prompt : z.string().optional(),
+  publicId : z.string(),
+});
+
+const TransformationForm = ({action,data=null}:TransformationFormProps) => {
+  const initialValues = data && action === 'Update' ?{
+    title: data?.title,
+    aspectRatio: data?.aspectRatio,
+    color: data?.color,
+    prompt: data?.prompt,
+    publicId: data?.publicId,
+  } : defaultValues
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
+    defaultValues: initialValues,
   });
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+   
     console.log(values);
   }
   return (
