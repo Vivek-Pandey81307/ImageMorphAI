e58b1600ce55d9478a2ebf28f2554f5f07f0326d
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-09 23:57:44 +0530

    TransformationForm.tsx

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { string, z } from "zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 import {
   Form,
   FormControl,
@@ -13,23 +21,34 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { defaultValues } from "@/constants";
-const formSchema = z.object({
-  title : z.string(),
-  aspectRatio : z.string().optional(),
-  color : z.string().optional(),
-  prompt : z.string().optional(),
-  publicId : z.string(),
+import { defaultValues, transformationTypes } from "@/constants";
+import { CustomField } from "./CustomField";
+import { useState } from "react";
+export const formSchema = z.object({
+  title: z.string(),
+  aspectRatio: z.string().optional(),
+  color: z.string().optional(),
+  prompt: z.string().optional(),
+  publicId: z.string(),
 });
 
-const TransformationForm = ({action,data=null}:TransformationFormProps) => {
-  const initialValues = data && action === 'Update' ?{
-    title: data?.title,
-    aspectRatio: data?.aspectRatio,
-    color: data?.color,
-    prompt: data?.prompt,
-    publicId: data?.publicId,
-  } : defaultValues
+const TransformationForm = ({
+  action,
+  data = null,userId,type,creditBalance
+}: TransformationFormProps) => {
+  const transformationType = transformationTypes[type]
+  const [image,setImage] = useState(data)
+  const [newTransformation,setNewTransformation] = useState<Transformations | null>(null)
+  const initialValues = 
+    data && action === "Update"
+      ? {
+          title: data?.title,
+          aspectRatio: data?.aspectRatio,
+          color: data?.color,
+          prompt: data?.prompt,
+          publicId: data?.publicId,
+        }
+      : defaultValues;
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -38,29 +57,44 @@ const TransformationForm = ({action,data=null}:TransformationFormProps) => {
 
   // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-   
     console.log(values);
   }
+  const onSelectFieldHandler =(
+    value: string ,  onChangeField: (value:string) =>void )=>{
+      
+    }
+  
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        <CustomField
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          name="title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({ field }) => <Input {...field} className="input-field" />}
         />
-        <Button type="submit">Submit</Button>
+
+        {type === "fill" && (
+          <CustomField
+            control={form.control}
+            name="aspectRatio"
+            formLabel="Aspect Ratio"
+            className="w-full"
+            render={({ field }) => (
+              <Select>
+                <SelectTrigger className="w-[180px]">
+                  <SelectValue placeholder="Theme" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="light">Light</SelectItem>
+                  <SelectItem value="dark">Dark</SelectItem>
+                  <SelectItem value="system">System</SelectItem>
+                </SelectContent>
+              </Select>
+            )}
+          />
+        )}
       </form>
     </Form>
   );
