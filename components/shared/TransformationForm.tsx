946c79af1b99d1271f2b1c49725f260357da3c87
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-18 16:03:29 +0530

    Generative fill -aspect ratio section

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -13,15 +13,29 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { defaultValues } from "@/constants";
-const formSchema = z.object({
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants";
+import { CustomField } from "./CustomField";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { useState } from "react";
+import { AspectRatioKey } from "@/lib/utils";
+
+export const formSchema = z.object({
     title:z.string(),
     aspectRatio:z.string().optional(),
     color : z.string().optional(),
     prompt : z.string().optional(),
     publicId : z.string(),
 });
-const TransformationForm = ({action,data=null}:TransformationFormProps) => {
+const TransformationForm = ({action,data=null,userId,type,creditBalance}:TransformationFormProps) => {
+  const transformationType= transformationTypes[type]
+  const [image,setImage] = useState(data);
+  const [ newTransformation ,setNewTransformation] = useState<Transformations | null>(null);
   const initialValues =  data && action === 'Update' ? {
     title: data?.title,
     aspectRatio: data?.aspectRatio,
@@ -41,26 +55,38 @@ const TransformationForm = ({action,data=null}:TransformationFormProps) => {
     // ✅ This will be type-safe and validated.
     console.log(values);
   }
+  const onSelectFieldHandler = (value: string, onChangeField :(value:string)=>void) =>{}
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
+        <CustomField 
           control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
+          name = "title"
+          formLabel="Image Title"
+          className="w-full"
+          render={({field})=><Input {...field} className="input-field"/>}
         />
-        <Button type="submit">Submit</Button>
+        {type==='fill' && (
+        <CustomField 
+        control={form.control}
+        name="aspectRatio"
+        formLabel="Aspect Ratio"
+        className="w-full"
+        render={({field})=>(
+          <Select  
+          onValueChange={(value)=>onSelectFieldHandler(value,field.onChange)}>
+  <SelectTrigger className="select-field">
+    <SelectValue placeholder="Select size" />
+  </SelectTrigger>
+  <SelectContent>
+    {Object.keys(aspectRatioOptions).map((key)=>(
+      <SelectItem key={key} value={key} className="select-item">{aspectRatioOptions[key as AspectRatioKey].label}</SelectItem>
+    ))}
+  </SelectContent>
+</Select>
+
+        )}/>
+        )}
       </form>
     </Form>
   );
