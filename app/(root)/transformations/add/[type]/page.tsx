7daeda9ec235ba9cf42db1ae657c886c3458f5ae
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-19 00:11:51 +0530

    button - transformation & save Image

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -14,12 +14,12 @@ const AddTransformationTypePage = async({params : {type}}:SearchParamProps)=>{
     <>
       <Header title={transformation.title}
     subtitle={transformation.subTitle}/>
-     <TransformationForm 
+    <section className='mt-10'> <TransformationForm 
       action='Add'
       userId={user._id}
       type = {transformation.type as TransformationTypeKey}
       creditBalance={user.creditBalance}
-     />
+     /> </section >
     </>
     
   )
