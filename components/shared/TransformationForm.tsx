9ce593c54421a8c2ca05c61313a9675101008619
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-03-18 23:47:12 +0530

    type-fill,recolor,remove & button configured

--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -24,6 +24,7 @@ import {
 } from "@/components/ui/select"
 import { useState } from "react";
 import { AspectRatioKey } from "@/lib/utils";
+import { config } from "process";
 
 export const formSchema = z.object({
     title:z.string(),
@@ -32,10 +33,14 @@ export const formSchema = z.object({
     prompt : z.string().optional(),
     publicId : z.string(),
 });
-const TransformationForm = ({action,data=null,userId,type,creditBalance}:TransformationFormProps) => {
+const TransformationForm = ({action,data=null,userId,type,creditBalance,config=null}:TransformationFormProps) => {
   const transformationType= transformationTypes[type]
   const [image,setImage] = useState(data);
+  
   const [ newTransformation ,setNewTransformation] = useState<Transformations | null>(null);
+  const [ isSubmitting,setIsSubmitting] =useState(false);
+  const  [ isTransforming,setIsTransforming] = useState(false)
+  const [transformationConfig,setTransformationConfig] = useState(config)
   const initialValues =  data && action === 'Update' ? {
     title: data?.title,
     aspectRatio: data?.aspectRatio,
@@ -56,6 +61,7 @@ const TransformationForm = ({action,data=null,userId,type,creditBalance}:Transfo
     console.log(values);
   }
   const onSelectFieldHandler = (value: string, onChangeField :(value:string)=>void) =>{}
+  const onInputChangeHandler = (fieldName:string,value:string,type:string,onChangeField:(value:string) =>void)=>{}
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -87,6 +93,41 @@ const TransformationForm = ({action,data=null,userId,type,creditBalance}:Transfo
 
         )}/>
         )}
+        {(type === 'remove' || type ==='recolor') && 
+          <div className="prompt-field">
+            <CustomField 
+            control={form.control}
+            name="prompt"
+            formLabel={
+              type === 'remove'?'Object to remove' : 'Object to recolor'
+            }
+            className="w-full"
+            render={({field})=>(
+              <Input 
+                value={field.value}
+                className="input-field"
+                onChange={(e)=>onInputChangeHandler('prompt',e.target.value,type,field.onChange)}
+              />
+            )}/>
+            {type==='recolor' && (
+              <CustomField 
+                control = {form.control}
+                name="color"
+                formLabel="Replacement Color"
+                className="w-full"
+                render={({field})=>(
+                  <Input 
+                  value={field.value}
+                  className="input-field"
+                  onChange={(e)=>onInputChangeHandler('color',e.target.value,type,field.onChange)}  
+                  />
+                )}
+              />
+            )}
+          </div>
+        }
+        <Button type="submit"
+        className="submit-button capitalize" disabled={isSubmitting}>Submit</Button>
       </form>
     </Form>
   );
