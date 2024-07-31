6c947c61404b11e1bc15b46d23180dd24ff24f29
Vivek-Pandey81307 <vs9595@srmist.edu.in>
2024-07-31 07:25:36 +0530

    Added Test Login Account

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+'use client'
+import React, { useEffect } from 'react'
 import { SignIn } from '@clerk/nextjs'
-const SignInPage = ()=>{
-  return <SignIn/>
+import { toast } from 'sonner'
+
+const SignInPage = () => {
+  useEffect(() => {
+    toast(
+      <h1 style={{ whiteSpace: 'pre-line' }}>
+        {`Test Mode\nEmail: codecrusaders02@gmail.com\nPassword: codecrusaders02`}
+      </h1>,
+      {
+        duration: 600000, // Duration in milliseconds
+      }
+    )
+  }, [])
+  return <SignIn />
 }
 
 export default SignInPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import { Toaster } from "@/components/ui/sonner";
 const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"],
 weight : ['400','500','600','700'],
 variable:'--font-ibm-plex' });
@@ -20,7 +22,7 @@ export default function RootLayout({
   return (
     <ClerkProvider appearance={{variables:{colorPrimary:'#624cf5'}}}>
     <html lang="en">
-      <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>{children}</body>
+      <body className={cn("font-IBMPlex antialiased",IBMPlex.variable)}>{children}<Toaster/></body>
     </html>
     </ClerkProvider>
   );
